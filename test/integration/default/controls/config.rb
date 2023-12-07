5e4eaf89a2f4a6c126aa29036f6036f11bd957a6
jeanluc <lkubb@protonmail.com>
2023-12-07 02:26:01 +0100

    fix: Update template
    This also fixes detection of Salt-SSH during orchestration and with
    saltutil.cmd

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'openssh.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"openssh-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"openssh--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"openssh-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"openssh--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
